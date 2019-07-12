e9db703db3a4d6c07e6e560236b2d8e1da763d47
zentim <zentim3248@gmail.com>
2019-07-13 01:02:32 +0800

    Add flow

--- src/pages/ItemFlow.vue
+++ src/pages/ItemFlow.vue
@@ -22,7 +22,8 @@
           <v-text-field label="Title..." solo outline flat hide-details></v-text-field>
         </v-flex>
         <v-flex d-flex xs12>
-          <v-layout row wrap>
+          <!-- type: item -->
+          <v-layout row wrap v-show="type === 'item'">
             <!-- outline -->
             <v-flex d-flex xs2>
               <v-textarea
@@ -37,6 +38,31 @@
             <!-- content -->
             <v-flex d-flex xs10>
               <tinymce-editor :init="{plugins: 'wordcount', height: '100%'}"></tinymce-editor>
+            </v-flex>
+          </v-layout>
+          <!-- type: flow -->
+          <v-layout row wrap v-show="type === 'flow'">
+            <!-- outline -->
+            <v-flex d-flex xs12>
+              <v-list two-line>
+                <template v-for="(item, index) in items">
+                  <v-list-tile :key="item.title" avatar ripple @click="toggle(index)">
+                    <v-list-tile-content>
+                      <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                      <v-list-tile-sub-title class="text--primary">{{ item.headline }}</v-list-tile-sub-title>
+                      <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+
+                    <v-list-tile-action>
+                      <v-list-tile-action-text>{{ item.action }}</v-list-tile-action-text>
+                      <v-icon v-if="selected.indexOf(index) < 0" color="grey lighten-1">star_border</v-icon>
+
+                      <v-icon v-else color="yellow darken-2">star</v-icon>
+                    </v-list-tile-action>
+                  </v-list-tile>
+                  <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+                </template>
+              </v-list>
             </v-flex>
           </v-layout>
         </v-flex>
@@ -61,9 +87,43 @@ export default {
     'tinymce-editor': Editor
   },
   data: () => ({
+    type: 'flow',
     title: '',
     outline: '',
-    content: ''
+    content: '',
+    selected: [2],
+    items: [
+      {
+        action: '15 min',
+        headline: 'Brunch this weekend?',
+        title: 'Ali Connors',
+        subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+      },
+      {
+        action: '2 hr',
+        headline: 'Summer BBQ',
+        title: 'me, Scrott, Jennifer',
+        subtitle: "Wish I could come, but I'm out of town this weekend."
+      },
+      {
+        action: '6 hr',
+        headline: 'Oui oui',
+        title: 'Sandra Adams',
+        subtitle: 'Do you have Paris recommendations? Have you ever been?'
+      },
+      {
+        action: '12 hr',
+        headline: 'Birthday gift',
+        title: 'Trevor Hansen',
+        subtitle: 'Have any ideas about what we should get Heidi for her birthday?'
+      },
+      {
+        action: '18hr',
+        headline: 'Recipe to try',
+        title: 'Britta Holt',
+        subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.'
+      }
+    ]
   })
 }
 </script>
