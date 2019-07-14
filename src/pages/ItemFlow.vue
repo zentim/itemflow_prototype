ee27c1d4cb0bde9216cd504a144a9b4717530b20
zentim <zentim3248@gmail.com>
2019-07-14 10:17:30 +0800

    Add draggable

--- src/pages/ItemFlow.vue
+++ src/pages/ItemFlow.vue
@@ -42,26 +42,32 @@
           </v-layout>
           <!-- type: flow -->
           <v-layout row wrap v-show="type === 'flow'">
-            <!-- outline -->
             <v-flex d-flex xs12>
               <v-list two-line>
-                <template v-for="(item, index) in items">
-                  <v-list-tile :key="item.title" avatar ripple @click="toggle(index)">
-                    <v-list-tile-content>
-                      <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                      <v-list-tile-sub-title class="text--primary">{{ item.headline }}</v-list-tile-sub-title>
-                      <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
-                    </v-list-tile-content>
+                <draggable v-model="myArray">
+                  <transition-group>
+                    <div v-for="(item, index) in items" :key="item.id">
+                      <v-list-tile :key="item.title" avatar ripple @click="toggle(index)">
+                        <v-list-tile-content>
+                          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                          <v-list-tile-sub-title class="text--primary">{{ item.headline }}</v-list-tile-sub-title>
+                          <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                        </v-list-tile-content>
 
-                    <v-list-tile-action>
-                      <v-list-tile-action-text>{{ item.action }}</v-list-tile-action-text>
-                      <v-icon v-if="selected.indexOf(index) < 0" color="grey lighten-1">star_border</v-icon>
+                        <v-list-tile-action>
+                          <v-list-tile-action-text>{{ item.action }}</v-list-tile-action-text>
+                          <v-icon
+                            v-if="selected.indexOf(index) < 0"
+                            color="grey lighten-1"
+                          >star_border</v-icon>
 
-                      <v-icon v-else color="yellow darken-2">star</v-icon>
-                    </v-list-tile-action>
-                  </v-list-tile>
-                  <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
-                </template>
+                          <v-icon v-else color="yellow darken-2">star</v-icon>
+                        </v-list-tile-action>
+                      </v-list-tile>
+                      <v-divider v-if="index + 1 < items.length" :key="index"></v-divider>
+                    </div>
+                  </transition-group>
+                </draggable>
               </v-list>
             </v-flex>
           </v-layout>
@@ -151,9 +157,12 @@
 </template>
 
 <script>
-
+import draggable from 'vuedraggable'
 export default {
   name: 'ItemFlow',
+  components: {
+    draggable,
+  },
   data: () => ({
     type: 'item',
     title: '',
@@ -164,30 +173,35 @@ export default {
     selected: [2],
     items: [
       {
+        id: '9',
         action: '15 min',
         headline: 'Brunch this weekend?',
         title: 'Ali Connors',
         subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
       },
       {
+        id: '10',
         action: '2 hr',
         headline: 'Summer BBQ',
         title: 'me, Scrott, Jennifer',
         subtitle: "Wish I could come, but I'm out of town this weekend."
       },
       {
+        id: '11',
         action: '6 hr',
         headline: 'Oui oui',
         title: 'Sandra Adams',
         subtitle: 'Do you have Paris recommendations? Have you ever been?'
       },
       {
+        id: '12',
         action: '12 hr',
         headline: 'Birthday gift',
         title: 'Trevor Hansen',
         subtitle: 'Have any ideas about what we should get Heidi for her birthday?'
       },
       {
+        id: '13',
         action: '18hr',
         headline: 'Recipe to try',
         title: 'Britta Holt',
